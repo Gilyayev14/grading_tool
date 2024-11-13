2d0034747c8b0c8d63712eeda90cc718d397eb14
Gilyayev14 <gabiilaev9@gmail.com>
2024-11-13 15:14:21 -0500

    Update testgaradecalculator.php

--- testgaradecalculator.php
+++ testgaradecalculator.php
@@ -1,31 +1,51 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+require_once __DIR__ . '/../src/GradingTool.php';
 
-require_once 'GradeCalculator.php';
-
-class GradeCalculatorTest extends TestCase {
-
-    public function testCalculateAverage() {
-        $scores = [75, 89, 103, 55, 100];
-        $avg = calculateAverage($scores);
-        $this->assertEquals(84.4, $avg);
+class GradingToolTest extends TestCase
+{
+    public function testAverageHomework()
+    {
+        $homeworks = [75, 89, 103, 55, 100];
+        $result = GradingTool::averageHomework($homeworks);
+        $this->assertEquals(84.4, $result);
     }
 
-    public function testDropLowestQuiz() {
-        $quizScores = [65, 78, 99, 76, 69];
-        $quizAvg = dropLowestQuiz($quizScores);
-        $this->assertEquals(80.5, $quizAvg);
+    public function testDropLowestQuiz()
+    {
+        $quizzes = [65, 78, 99, 76, 69];
+        $result = GradingTool::dropLowestQuiz($quizzes);
+        $this->assertEquals([69, 76, 78, 99], $result);
     }
 
-    public function testCalculateFinalGrade() {
-        $homeworkAvg = 84.4;
-        $quizAvg = 80.5;
+    public function testCalculateFinalGrade()
+    {
+        $homeworks = [75, 89, 103, 55, 100];
+        $quizzes = [65, 78, 99, 76, 69];
         $midterm = 86;
         $finalProject = 90;
 
-        $finalGrade = calculateFinalGrade($homeworkAvg, $quizAvg, $midterm, $finalProject);
+        $finalGrade = GradingTool::calculateFinalGrade($homeworks, $quizzes, $midterm, $finalProject);
         $this->assertEquals(87, $finalGrade);
     }
+
+    public function testEdgeCases()
+    {
+        $homeworks = [0, 0, 0, 0, 0];
+        $quizzes = [0, 0, 0, 0, 0];
+        $midterm = 0;
+        $finalProject = 0;
+
+        $finalGrade = GradingTool::calculateFinalGrade($homeworks, $quizzes, $midterm, $finalProject);
+        $this->assertEquals(0, $finalGrade);
+
+        $homeworks = [100, 100, 100, 100, 100];
+        $quizzes = [100, 100, 100, 100, 100];
+        $midterm = 100;
+        $finalProject = 100;
+
+        $finalGrade = GradingTool::calculateFinalGrade($homeworks, $quizzes, $midterm, $finalProject);
+        $this->assertEquals(100, $finalGrade);
+    }
 }
-?>
